b9dc57f07364d1796c9c9d20ffcfb465afe43e85
KonTy <9524513+KonTy@users.noreply.github.com>
2023-11-29 20:17:39 -0800

    Update curl-install.sh

--- curl-install.sh
+++ curl-install.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-SCRIPTHOME_DIR=archi
-# Checking if is running in Repo Folder
-if [[ "$(basename "$(pwd)" | tr '[:upper:]' '[:lower:]')" =~ ^scripts$ ]]; then
-    echo "You are running this in ${SCRIPTHOME_DIR} Folder."
-    exit
-fi
 
 # Installing git
 cd ~
 echo "Installing git."
 pacman -Sy --noconfirm --needed git glibc
 
-echo "Cloning the $SCRIPTHOME_DIR Project"
+echo "Cloning the archi Project"
 git clone https://github.com/KonTy/archi
 
 echo "Change directory to ~/$SCRIPTHOME_DIR"
-cd ~/$SCRIPTHOME_DIR
+cd $HOME/archi
 
 echo "Current directory is $(pwd) Executing Script"
 exec ./archi.sh
