c767198cec7feea9c862f89638d86032f1242231
KonTy <9524513+KonTy@users.noreply.github.com>
2023-11-29 18:39:47 -0800

    Update curl-install.sh

--- curl-install.sh
+++ curl-install.sh
@@ -14,9 +14,8 @@ pacman -Sy --noconfirm --needed git glibc
 echo "Cloning the ${SCRIPTHOME_DIR} Project"
 git clone https://github.com/KonTy/archi
 
-echo "Change directory to $HOME/$SCRIPT_DIR"
-cd "$HOME/$SCRIPT_DIR"
-
+echo "Change directory to $HOME/$SCRIPTHOME_DIR"
+cd "$HOME/$SCRIPTHOME_DIR"
 
 echo "Current directory is $pwd Executing ${SCRIPTHOME_DIR} Script"
 exec ./archi.sh
