4649eaa60f56ec5ddb6cef366199309f3a426336
KonTy <KonTy@github.com>
2023-12-01 23:50:16 -0800

    trt

--- run_archi.sh
+++ run_archi.sh
@@ -9,27 +9,42 @@ set +a
 
 echo "All variables are set"
 
-log_to_file() {
-    local log_file="all-scripts.log"
 
-    awk -v script="$(basename "$0")" '{ print strftime("[%Y-%m-%d %H:%M:%S]"), "[" script "]:", $0 }' >> "$log_file"
-}
+# Output file
+output_file="output.log"
 
 echo "Script dir is $SCRIPT_DIR cd into it"
 cd "$SCRIPT_DIR"
 
 echo "Launching start up..."
-(bash $SCRIPTS_DIR/startup.sh) #2>&1 | log_to_file
+bash $SCRIPTS_DIR/startup.sh 2>&1 | tee -a "$output_file"
 source $CONFIGS_DIR/setup.conf
-(bash $SCRIPTS_DIR/0-preinstall.sh) #2>&1 | log_to_file
-
-# now modify actual installation
-(arch-chroot /mnt "$SCRIPTS_DIR/1-setup.sh") #2>&1 | log_to_file
+bash $SCRIPTS_DIR/0-preinstall.sh 2>&1 | tee -a "$output_file"
+arch-chroot /mnt "$SCRIPTS_DIR/1-setup.sh" 2>&1 | tee -a "$output_file"
 
 echo "Desktop Environment is $DESKTOP_ENV"
 if [[ "$DESKTOP_ENV" != "server" ]]; then
- (arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- $SCRIPTS_DIR/2-user.sh ) #|& log_to_file
+    (arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- $SCRIPTS_DIR/2-user.sh) 2>&1 | tee -a "$output_file"
 fi
+(arch-chroot /mnt "$SCRIPTS_DIR/3-post-setup.sh") 2>&1 | tee -a "$output_file"
 
-(arch-chroot /mnt "$SCRIPTS_DIR/3-post-setup.sh") # |& log_to_file
+
+
+
+
+
+
+# (bash $SCRIPTS_DIR/startup.sh) #2>&1 | log_to_file
+# source $CONFIGS_DIR/setup.conf
+# (bash $SCRIPTS_DIR/0-preinstall.sh) #2>&1 | log_to_file
+
+# now modify actual installation
+# (arch-chroot /mnt "$SCRIPTS_DIR/1-setup.sh") #2>&1 | log_to_file
+
+# echo "Desktop Environment is $DESKTOP_ENV"
+# if [[ "$DESKTOP_ENV" != "server" ]]; then
+#  (arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- $SCRIPTS_DIR/2-user.sh ) #|& log_to_file
+# fi
+
+# (arch-chroot /mnt "$SCRIPTS_DIR/3-post-setup.sh") # |& log_to_file
 #cp -v *.log /mnt/home/$USERNAME
