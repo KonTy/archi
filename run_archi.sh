51dd04fd293c0130cc2c6eb82508fe7d4cfead8c
KonTy <KonTy@github.com>
2023-12-02 12:38:47 -0800

    asd

--- run_archi.sh
+++ run_archi.sh
@@ -23,29 +23,13 @@ source $CONFIGS_DIR/setup.conf
 ( arch-chroot /mnt $SCRIPTS_DIR/1-setup.sh ) 2>&1 | tee -a "$output_file"
 
 echo "Desktop Environment is $DESKTOP_ENV" 2>&1 | tee -a "$output_file"
+chmod +x $SCRIPTS_DIR/2-user.sh
 if [[ "$DESKTOP_ENV" != "server" ]]; then
     echo "Running as $USERNAME script: $SCRIPTS_DIR/2-user.sh" 2>&1 | tee -a "$output_file"
-    ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- $SCRIPTS_DIR/2-user.sh ) 2>&1 | tee -a "$output_file"
+    if ! (arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- $SCRIPTS_DIR/2-user.sh) > >(tee -a "$output_file") 2> >(tee -a "$output_file" >&2); then
+        echo "Permission denied error. Check $output_file for details."
+        exit
+    fi
+#    ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- $SCRIPTS_DIR/2-user.sh ) 2>&1 | tee -a "$output_file"
 fi
 ( arch-chroot /mnt $SCRIPTS_DIR/3-post-setup.sh ) 2>&1 | tee -a "$output_file"
-
-
-
-
-
-
-
-# (bash $SCRIPTS_DIR/startup.sh) #2>&1 | log_to_file
-# source $CONFIGS_DIR/setup.conf
-# (bash $SCRIPTS_DIR/0-preinstall.sh) #2>&1 | log_to_file
-
-# now modify actual installation
-# (arch-chroot /mnt "$SCRIPTS_DIR/1-setup.sh") #2>&1 | log_to_file
-
-# echo "Desktop Environment is $DESKTOP_ENV"
-# if [[ "$DESKTOP_ENV" != "server" ]]; then
-#  (arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- $SCRIPTS_DIR/2-user.sh ) #|& log_to_file
-# fi
-
-# (arch-chroot /mnt "$SCRIPTS_DIR/3-post-setup.sh") # |& log_to_file
-#cp -v *.log /mnt/home/$USERNAME
