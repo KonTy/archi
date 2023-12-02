9e9f812f5cee469969be8446a59d63394e2b8723
KonTy <KonTy@github.com>
2023-12-02 11:52:15 -0800

    asd

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -199,25 +199,57 @@ filesystemOld () {
 # @description This function will handle file systems. At this movement we are handling only
 # btrfs and ext4. Others will be added in future.
 filesystem () {
-    options=("btrfs" "ext4" "luks" "exit")
-    choice=$(dialog --menu "Choose Filesystem:" 10 30 4 "${options[@]}" 2>&1 >/dev/tty)
-
-    case $? in
-    0)
+    cmd=(dialog --output-fd 1 --separate-output --extra-button --extra-label 'Select All' --cancel-label 'Select None' --checklist 'Choose the tools to install:' 0 0 0)
+    load-dialog () {
+        options=(
+                    1 'btrfs' off
+                    2 'ext4' on
+                    3 'luks' off
+        )
+        choices=$("${cmd[@]}" "${options[@]}")
+    }
+    load-dialog
+    exit_code="$?"
+    for choice in $choices
+    do
         case $choice in
-        0) set_option FS btrfs ;;
-        1) set_option FS ext4 ;;
-        2)
-            set_password "LUKS_PASSWORD"
-            set_option FS luks
-            ;;
-        3) exit ;;
-        *) echo "Wrong option please select again"; filesystem ;;
+            1) echo 'First Option';;
+            2) echo 'Second Option';;
+            3) echo 'Third Option';;
         esac
-        ;;
-    1) echo "Canceled." ;;
-    *) echo "Error or Escape key pressed. Exiting." ;;
-    esac
+    done
+pause
+
+
+
+
+
+
+
+
+
+
+
+
+    # options=("btrfs" "ext4" "luks" "exit")
+    # choice=$(dialog --menu "Choose Filesystem:" 10 30 4 "${options[@]}" 2>&1 >/dev/tty)
+
+    # case $? in
+    # 0)
+    #     case $choice in
+    #     0) set_option FS btrfs ;;
+    #     1) set_option FS ext4 ;;
+    #     2)
+    #         set_password "LUKS_PASSWORD"
+    #         set_option FS luks
+    #         ;;
+    #     3) exit ;;
+    #     *) echo "Wrong option please select again"; filesystem ;;
+    #     esac
+    #     ;;
+    # 1) echo "Canceled." ;;
+    # *) echo "Error or Escape key pressed. Exiting." ;;
+    # esac
 }
 
 
